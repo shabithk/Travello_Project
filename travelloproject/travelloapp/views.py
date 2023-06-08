18aed4b7b09ef7ab93257e5dcff1186991ee7d8a
shabithk <shabu233@gmail.com>
2023-06-08 20:44:00 +0530

    success

---
+++ travelloproject/travelloapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Place, People
+
+
+# Create your views here.
+def demo(request):
+      obj = Place.objects.all()
+      obj1 = People.objects.all()
+      return render(request,"index.html",{'result':obj,'output':obj1})
+
